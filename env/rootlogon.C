a6472dd8c3faa2a441c6c5cf207f379a4318a493
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2017-09-27 16:46:39 +0300

    rootlogon.C :: just load PWG libs that will pull dependencies

--- env/rootlogon.C
+++ env/rootlogon.C
@@ -2,13 +2,13 @@
 gSystem->AddIncludePath("-Wno-deprecated -I$. -I/usr/include -I$FASTJET/include -I$ROOTSYS/include -I$ALICE_ROOT/include -I$ALICE_PHYSICS/include");
 gSystem->AddDynamicPath("/usr/lib64/ $FASTJET/lib/ $ROOTSYS/lib/ $ALICE_ROOT/lib/ $ALICE_PHYSICS/lib/");
 
-gSystem->Load("/usr/lib64/libCGAL");
-gSystem->Load("libfastjet");
-gSystem->Load("libsiscone");
-gSystem->Load("libsiscone_spherical");
-gSystem->Load("libfastjetplugins");
-gSystem->Load("libfastjettools");
-gSystem->Load("libfastjetcontribfragile");
+//gSystem->Load("/usr/lib64/libCGAL");
+//gSystem->Load("libfastjet");
+//gSystem->Load("libsiscone");
+//gSystem->Load("libsiscone_spherical");
+//gSystem->Load("libfastjetplugins");
+//gSystem->Load("libfastjettools");
+//gSystem->Load("libfastjetcontribfragile");
 gSystem->Load("libPWGJE");
 gSystem->Load("libPWGJEEMCALJetTasks");
 //gSystem->ListLibraries();
