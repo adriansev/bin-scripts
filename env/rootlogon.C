56cc71fce7f50c2da3102cb5540ca527de5c07aa
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2019-03-14 01:50:36 +0200

    resolve conflict

--- env/rootlogon.C
+++ env/rootlogon.C
@@ -1,18 +1,10 @@
 {
-gSystem->AddIncludePath("-Wno-deprecated -I. -I$ALICE_PHYSICS/include -I$ALICE_ROOT/include -I$FASTJET/include -I/usr/include");
-gSystem->AddDynamicPath("$ALICE_PHYSICS/lib:$ALICE_ROOT/lib:$ROOTSYS/lib:$FASTJET/lib:/usr/lib64");
+gSystem->AddIncludePath("-Wno-deprecated -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-reorder");
+gSystem->AddIncludePath("-I. -I$ALICE_PHYSICS/include -I$ALICE_ROOT/include -I$FASTJET/include -I/usr/include");
+gSystem->AddDynamicPath(".:$ALICE_PHYSICS/lib:$ALICE_ROOT/lib:$ROOTSYS/lib:$FASTJET/lib");
 
-//gSystem->Load("/usr/lib64/libCGAL");
-//gSystem->Load("libfastjet");
-//gSystem->Load("libsiscone");
-//gSystem->Load("libsiscone_spherical");
-//gSystem->Load("libfastjetplugins");
-//gSystem->Load("libfastjettools");
-//gSystem->Load("libfastjetcontribfragile");
 gSystem->Load("libPWGPP");
 gSystem->Load("libPWGJEEMCALJetTasks");
 //gSystem->ListLibraries();
 }
-
-
 
