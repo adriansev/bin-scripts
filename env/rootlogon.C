3544bfc41d7a275a9926fae2ee1360506302644c
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2019-03-13 14:25:42 +0200

    rootlogon.C update

--- env/rootlogon.C
+++ env/rootlogon.C
@@ -1,6 +1,6 @@
 {
-gSystem->AddIncludePath("-Wno-deprecated -I$. -I$ALICE_PHYSICS/include -I$ALICE_ROOT/include -I$FASTJET/include -I/usr/include");
-//gSystem->AddDynamicPath("$ALICE_PHYSICS/lib:$ALICE_ROOT/lib:$ROOTSYS/lib:$FASTJET/lib:/usr/lib64");
+gSystem->AddIncludePath("-Wno-deprecated -I. -I$ALICE_PHYSICS/include -I$ALICE_ROOT/include -I$FASTJET/include -I/usr/include");
+gSystem->AddDynamicPath("$ALICE_PHYSICS/lib:$ALICE_ROOT/lib:$ROOTSYS/lib:$FASTJET/lib:/usr/lib64");
 
 //gSystem->Load("/usr/lib64/libCGAL");
 //gSystem->Load("libfastjet");
@@ -9,7 +9,7 @@ gSystem->AddIncludePath("-Wno-deprecated -I$. -I$ALICE_PHYSICS/include -I$ALICE_
 //gSystem->Load("libfastjetplugins");
 //gSystem->Load("libfastjettools");
 //gSystem->Load("libfastjetcontribfragile");
-//gSystem->Load("libPWGJE");
+gSystem->Load("libPWGPP");
 gSystem->Load("libPWGJEEMCALJetTasks");
 //gSystem->ListLibraries();
 }
