a086baaaf0da305545fd292a258c73069b24e654
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2024-07-05 16:46:14 +0200

    update ccdb_ commands

--- ccdb_cpv_noise.py
+++ ccdb_cpv_noise.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 import sys
+from rich.console import Console
+from rich.table import Table
 
 # import alienpy functions
 try:
-    from alienpy.wb_api import PrintDict, retf_print
+    from alienpy.wb_api import retf_print
+    from alienpy.tools_nowb import PrintDict, unixtime2local
     from alienpy.alien import *  # nosec PYL-W0614
 except Exception:
     try:
-        from xjalienfs.wb_api import PrintDict, retf_print
+        from xjalienfs.wb_api import retf_print
+        from xjalienfs.tools_nowb import PrintDict, unixtime2local
         from xjalienfs.alien import *  # nosec PYL-W0614
     except Exception:
         print("Can't load alienpy, exiting...")
         sys.exit(1)
-
-from rich.console import Console
-from rich.table import Table
 
 ########################################
 ##   REQUIRED INITIAILZATION
@@ -39,7 +40,7 @@ for obj in obj_list:
     rez = DO_ccdb_query([obj])
     for q in rez.ansdict['objects']:
         run = q.get("runNumber", '-1')
-        table.add_row(q["path"].replace(common_dir,""), run, str(q["Content-Length"]), q["Last-Modified"], q["Valid-Until"])
+        table.add_row(q["path"].replace(common_dir,""), run, str(q["Content-Length"]), unixtime2local(q["Last-Modified"]), unixtime2local(q["Valid-Until"]))
 
 console = Console()
 console.print(table)
